ae29f33579eaec4ea38c95f10f0cd2bf004ecd13
Joshua Beam <joshuabeam@Joshuas-MacBook-Pro.local>
2015-03-03 22:39:52 -0600

    fixed indentation

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var livereload = require('express-livereload');
 var app = express();
+
+livereload(app, config={});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
