a984c06d382694b4ed972e00d7470e59de5037eb
Joshua Beam <joshuabeam@Joshuas-MacBook-Pro.local>
2015-03-04 16:40:41 -0600

    favicon change

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ livereload(app, config={});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
